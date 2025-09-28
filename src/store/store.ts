a52e8295bff98a55dcbc3ddb5c407625d6de5170
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2025-09-28 15:04:27 +0200

    setting up store.ts

---
+++ src/store/store.ts
@@ -0,0 +1,9 @@
+/**
+ *  The state object is defined here.
+ * (eg. state = { isLoggedIn: false, userProfile: null })
+ */
+
+export const state = { 
+  isLoggedIn: false, 
+  userProfile: null as { name: string } | null, 
+};
