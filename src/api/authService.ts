fbc771623577380b73f839f9804cfcf9d10f9f66
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2025-10-03 11:18:49 +0200

    Refactor authentication functions to use structured interfaces for login and registration

--- src/api/authService.ts
+++ src/api/authService.ts
@@ -4,23 +4,83 @@
  */
 
 import { post } from "./apiClient";
+import { setAccessToken } from "../utils/authUtils";
 
-export async function login(email, password) {
 
-  try {
-    const response = await post('/auth/login', { email, password });
-    return response;
-  } catch (error) {
-    console.error('Login failed:', error);
-  }
-  
+export interface RegisterBody {
+  name: string; // Required
+  email: string; // Required
+  password: string; // Required
+  bio?: string; // Optional
+  avatar?: {
+    url: string; // Optional
+    alt: string; // Optional
+  };
+  banner?: {
+    url: string; // Optional
+    alt: string; // Optional
+  };
+  venueManager: boolean; // Optional
 }
 
-export async function register(name, email, password) {
+
+export interface LoginCredentials {
+ email: string;
+ password: string;
+}
+
+export interface UserProfile {
+    name: string;
+    email: string;
+    bio?: string;
+    avatar?: {
+      url: string;
+      alt: string;
+    } | null;
+    banner?: {
+      url: string;
+      alt: string;
+    } | null;
+    venueManager: boolean;
+    id?: number;
+}
+
+export interface AuthResponseData extends UserProfile {
+  accessToken: string;
+}
+
+export async function login(credentials: LoginCredentials): Promise<UserProfile | undefined> {
+
   try {
-    const response = await post('/auth/register', { name, email, password });
-    return response;
+
+    const response = await post<AuthResponseData, LoginCredentials>('/auth/login', credentials);
+  
+    if (response && response.data.accessToken) {
+      setAccessToken(response.data.accessToken);
+      const { accessToken, ...userProfile } = response.data;
+      return userProfile as UserProfile;
+    } 
+  } catch (error) {
+    console.error('Login failed:', error);
+    throw error;  
+  }
+}
+
+export async function register(body: RegisterBody): Promise<UserProfile | undefined> {
+
+  try {
+
+    const response = await post<AuthResponseData, RegisterBody>('/auth/register', body);
+    
+    if (response && response.data.accessToken) {
+      setAccessToken(response.data.accessToken);
+
+      const { accessToken, ...userProfile } = response.data;
+
+      return userProfile as UserProfile;
+    }
   } catch (error) {
     console.error('Register failed:', error);
+    throw error;
   }
 }
