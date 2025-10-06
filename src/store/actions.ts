5188526e9e56558b032f30f36669d08a3b819ac6
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2025-10-06 12:51:23 +0200

    Refactor actions.ts: streamline login and logout functions, enhance checkAuthStatus for token validation

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -3,24 +3,64 @@
  */
 
 import { state } from './store.ts'; 
-import { Router } from '../router/router.ts'
+import { Router } from '../router/router.ts';
+
+import type { LoginCredentials, UserProfile } from '../api/authService.ts';
+import { login as authLogin } from '../api/authService.ts';
+import { clearAccessToken, getAccessToken } from '../utils/authUtils.ts';
+import { getProfile } from '../api/profileService.ts';
  
-type UserProfile = { name: string };
 
 let router: Router | null = null; 
 
 export function setRouter(routerInstance: Router): void { 
   router = routerInstance; 
 } 
- 
-export function login(user: UserProfile): void { 
-  state.isLoggedIn = true; 
-  state.userProfile = user; 
-  router?.resolveRoute();
+
+function navigate(path: string): void {
+  router?.navigate(path);
+}
+
+export async function checkAuthStatus(): Promise<void> {
+  const token = getAccessToken();
+
+  if (token) {
+    try {
+      const profile = await getProfile();
+
+      if (profile) {
+        state.userProfile = profile;
+        state.isLoggedIn = true;
+        router?.resolveRoute();
+      }
+    } catch (error) {
+      clearAccessToken();
+      state.isLoggedIn = false;
+      console.error('Token validation failed. Cleared token.', error);
+    }
+  }
+
+}
+
+export async function login(credentials: LoginCredentials): Promise<void> { 
+try {
+  const profile = await authLogin(credentials);
+
+  if (profile) {
+    state.userProfile = profile;
+    state.isLoggedIn = true;
+    navigate('/');
+  }
+} catch (error) {
+  throw error;
+}
 } 
  
 export function logout(): void { 
+  clearAccessToken();
   state.isLoggedIn = false; 
   state.userProfile = null; 
   router?.resolveRoute(); // Re-render again. 
+
+  navigate('/login');
 } 
