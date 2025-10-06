ec592882a45ea923dd5801d14e59ee580d880441
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2025-10-06 12:51:47 +0200

    Refactor store.ts: update userProfile type to use UserProfile interface from authService

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,8 +2,9 @@
  *  The state object is defined here.
  * (eg. state = { isLoggedIn: false, userProfile: null })
  */
+import type { UserProfile } from "../api/authService";
 
 export const state = { 
   isLoggedIn: false, 
-  userProfile: null as { name: string } | null, 
+  userProfile: null as UserProfile | null, 
 };
